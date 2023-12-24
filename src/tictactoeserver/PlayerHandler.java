ad5822ecf3d15a7f8a79e451db46308983565b38
Ghoneim <ahmed.ghoneim1011@gmail.com>
2023-12-24 12:15:29 +0200

    adding avaliable users server logic

--- src/tictactoeserver/PlayerHandler.java
+++ src/tictactoeserver/PlayerHandler.java
@@ -14,6 +14,8 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.security.NoSuchAlgorithmException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Vector;
@@ -24,17 +26,22 @@ import java.util.logging.Logger;
  *
  * @author s
  */
-public class PlayerHandler extends Thread{
+public class PlayerHandler extends Thread {
+
+    ArrayList<Player> avaliablePlayerList;
+    static PreparedStatement psAvaliableUsers;
     Player playerData;
     DataInputStream dis;
     PrintStream ps;
     Socket socket;
     public static Vector<PlayerHandler> playersConnections;
+
     static {
         playersConnections = new Vector<PlayerHandler>();
     }
-    
-    public PlayerHandler(Socket socket){
+
+    public PlayerHandler(Socket socket) {
+        avaliablePlayerList = new ArrayList<Player>();
         try {
             this.socket = socket;
             dis = new DataInputStream(socket.getInputStream());
@@ -45,16 +52,17 @@ public class PlayerHandler extends Thread{
         playersConnections.add(this);
         this.start();
     }
+
     @Override
     public void run() {
-        while(true){
+        while (true) {
             String msgArray;
             try {
                 msgArray = dis.readLine();
                 Gson gson = new GsonBuilder().create();
                 ArrayList<String> messages = gson.fromJson(msgArray, ArrayList.class);
                 String msg = messages.get(0);
-                switch(msg){
+                switch (msg) {
                     case "login":
                         loginUser(messages.get(1));
                         break;
@@ -74,29 +82,29 @@ public class PlayerHandler extends Thread{
                         logout();
                         break;
                 }
-                
+
             } catch (IOException ex) {
                 Logger.getLogger(PlayerHandler.class.getName()).log(Level.SEVERE, null, ex);
                 closeConnections();
             }
         }
     }
-    void closeConnections(){
-        try{
+
+    void closeConnections() {
+        try {
             socket.close();
             dis.close();
             ps.close();
             this.stop();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-    private void loginUser(String loginData){
+
+    private void loginUser(String loginData) {
         try {
             Player player = Login.checkLoginData(loginData);
-            if(player != null){
+            if (player != null) {
                 ArrayList<String> response = new ArrayList<>();
                 response.add("login");
                 response.add("Success");
@@ -105,15 +113,14 @@ public class PlayerHandler extends Thread{
                 response.add(playerJson);
                 String responseJSon = gson.toJson(response);
                 ps.println(responseJSon);
-            }
-            else{
+            } else {
                 ArrayList<String> response = new ArrayList<>();
                 response.add("login");
                 response.add("Failure");
                 Gson gson = new GsonBuilder().create();
                 String responseJSon = gson.toJson(response);
                 ps.println(responseJSon);
-                
+
             }
         } catch (SQLException ex) {
             Logger.getLogger(PlayerHandler.class.getName()).log(Level.SEVERE, null, ex);
@@ -121,30 +128,30 @@ public class PlayerHandler extends Thread{
             Logger.getLogger(PlayerHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    private void signUpUser(){
+
+    private void signUpUser() {
         //TODO: put signup code here
         //Recommended: put most of logic in seperate class with static methods
     }
-    
-    private void requestToPlay(ArrayList<String> request){
+
+    private void requestToPlay(ArrayList<String> request) {
         String player2UserName = request.get(1);
-        playersConnections.forEach(player ->{
-            if(player.playerData.getUserName().equals(player2UserName)){
+        playersConnections.forEach(player -> {
+            if (player.playerData.getUserName().equals(player2UserName)) {
                 ArrayList<String> response = new ArrayList<>();
                 response.add("request");
                 response.add(this.playerData.getUserName());
                 Gson gson = new GsonBuilder().create();
                 String responseJSon = gson.toJson(response);
                 player.ps.println(responseJSon);
-            }  
+            }
         });
     }
-    
-    private void startGame(ArrayList<String> request){
+
+    private void startGame(ArrayList<String> request) {
         String player1UserName = request.get(1);
-        playersConnections.forEach(player ->{
-            if(player.playerData.getUserName().equals(player1UserName)){
+        playersConnections.forEach(player -> {
+            if (player.playerData.getUserName().equals(player1UserName)) {
                 ArrayList<String> response1 = new ArrayList<>();
                 ArrayList<String> response2 = new ArrayList<>();
                 response1.add("startGame");
@@ -156,22 +163,40 @@ public class PlayerHandler extends Thread{
                 String responseJSon2 = gson.toJson(response2);
                 player.ps.println(responseJSon1);
                 this.ps.println(responseJSon2);
-                
+
                 Game game = new Game(player, this);
-            }  
+            }
         });
     }
-    
-    
-    
-    private void getAvailableUsers(){
-        //TODO: put available users code here
-        //Recommended: put most of logic in seperate class with static methods
+
+    private void getAvailableUsers() {
+        try {
+            psAvaliableUsers = ServerConnection.con.prepareStatement("SELECT * FROM PLAYER WHERE AVALIABLE=TRUE and isplaying=FALSE");
+            ResultSet rs = psAvaliableUsers.executeQuery();
+            while (rs.next()) {
+                Player p1 = new Player();
+                p1.setUserName(rs.getString("username"));
+                p1.setEmail(rs.getString("email"));
+                p1.setPlayerImage(rs.getString("playerimage"));
+                p1.setScore(rs.getLong("score"));
+                avaliablePlayerList.add(p1);
+               
+            }
+           ArrayList<String> response = new ArrayList<>();
+             Gson gson = new GsonBuilder().create();
+                response.add("AvaliableUsers");
+                String playerJson = gson.toJson(avaliablePlayerList);
+                response.add(playerJson);
+                String responseJSon = gson.toJson(response);
+                ps.println(responseJSon);
+        } catch (SQLException ex) {
+            Logger.getLogger(PlayerHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    private void logout(){
+
+    private void logout() {
         //TODO: put logout code here
         //Recommended: put most of logic in seperate class with static methods
     }
-    
+
 }
